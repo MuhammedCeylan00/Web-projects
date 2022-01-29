7a2f33ccc3ebaf22ee95493e4ed6f4767c9e89b6
Muhammed ceylan <72267252+MuhammedCeylan00@users.noreply.github.com>
2022-01-29 15:16:51 +0300

    Add files via upload
    ozturk kombiye yaptığım reklam amaçlı web sitesi

---
+++ ÖztürkKombiTeknikServis/js/script.js
@@ -0,0 +1,7 @@
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menuBtn').onclick = () => {
+    navbar.classList.toggle('active');
+}
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
